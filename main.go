e728ce508008dacffe3a3cc938ecd2a797e76638
DQNEO <dqneoo@gmail.com>
2019-08-12 11:18:25 +0900

    use input from stdin

--- main.go
+++ main.go
@@ -1,10 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+	"strconv"
+)
 
 func main() {
+	var source []byte
+	source, _ = ioutil.ReadFile("/dev/stdin")
+	var input string = string(source)
+
+	number, err := strconv.Atoi(input)
+	if err != nil {
+		panic(err)
+	}
 	fmt.Printf("  .global main\n")
 	fmt.Printf("main:\n")
-	fmt.Printf("  movq $42, %%rax\n")
+	fmt.Printf("  movq $%d, %%rax\n", number)
 	fmt.Printf("  ret\n")
 }
