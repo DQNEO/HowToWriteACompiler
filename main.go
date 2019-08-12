307aa85a36bf13002d101236abf691c0319c1961
DQNEO <dqneoo@gmail.com>
2019-08-12 14:29:30 +0900

    Refactor

--- main.go
+++ main.go
@@ -94,10 +94,14 @@ func parse() *Expr {
 	return expr
 }
 
-func generate(expr *Expr) {
+func generateExpr(expr *Expr) {
+	fmt.Printf("  movq $%d, %%rax\n", expr.intval)
+}
+
+func generateCode(expr *Expr) {
 	fmt.Printf("  .global main\n")
 	fmt.Printf("main:\n")
-	fmt.Printf("  movq $%d, %%rax\n", expr.intval)
+	generateExpr(expr)
 	fmt.Printf("  ret\n")
 }
 
@@ -105,5 +109,5 @@ func main() {
 	source, _ = ioutil.ReadFile("/dev/stdin")
 	tokens = tokenize()
 	expr := parse()
-	generate(expr)
+	generateCode(expr)
 }
