05e380752ef257b6f4264fcd5c8547ce18f89246
DQNEO <dqneoo@gmail.com>
2019-08-12 13:10:04 +0900

    tokenize number literal

--- main.go
+++ main.go
@@ -1,36 +1,75 @@
 package main
 
 import (
+	"errors"
 	"fmt"
-	"strconv"
 	"io/ioutil"
-	)
+	"strconv"
+)
 
 type Token struct {
 	Type string // "numberliteral"
 	Value string
 }
 
-func tokenize(bytes []byte) []*Token {
+var sourceIndex = 0
+
+func getchar() (byte, error) {
+	if len(bytes) == sourceIndex {
+		return 0, errors.New("EOF")
+	}
+	char := bytes[sourceIndex]
+	sourceIndex++
+	return char, nil
+}
+
+func ungethar() {
+	sourceIndex--
+}
+
+func tokenize() []*Token {
 	var tokens []*Token
 
-	var token *Token
-	char := bytes[0]
-	if '0' <= char && char <= '9' {
-		token = &Token{
-			Type:"numberliteral",
-			Value: string(bytes),
+	for {
+		char, err := getchar()
+		if err != nil {
+			break
 		}
+		switch char {
+		case '0','1','2','3','4','5','6','7','8','9':
+			var number []byte = []byte{char}
+			for {
+				char, err := getchar()
+				if err != nil {
+					break
+				}
+				if '0' <= char && char <= '9' {
+					number = append(number, char)
+				} else {
+					ungethar()
+					break
+				}
+			}
+			token := &Token{
+				Type:"numberliteral",
+				Value: string(number),
+			}
+			tokens = append(tokens, token)
+		default:
+			panic(fmt.Sprintf("Invalid char: %c", char))
+		}
+
 	}
 
-	tokens = append(tokens, token)
+
 	return tokens
 }
 
+var bytes []byte
 func main() {
 	var err error
-	bytes, _ := ioutil.ReadFile("/dev/stdin")
-	tokens := tokenize(bytes)
+	bytes, _ = ioutil.ReadFile("/dev/stdin")
+	tokens := tokenize()
 
 	token := tokens[0]
 	number, err := strconv.Atoi(token.Value)
