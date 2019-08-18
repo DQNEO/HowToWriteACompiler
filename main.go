7afa3a066ce0088c3b969d1c3e270e3e9b4a6678
DQNEO <dqneoo@gmail.com>
2019-08-19 01:02:44 +0900

    Improve

--- main.go
+++ main.go
@@ -171,10 +171,10 @@ func generateExpr(expr *Expr) {
 		case "*":
 			fmt.Printf("  imulq %%rbx, %%rax\n")
 		default:
-			panic("generator: unknown operator:" + expr.operator)
+			panic("generator: Unknown operator:" + expr.operator)
 		}
 	default:
-		panic("generator: unknown expr type:" + expr.kind)
+		panic("generator: Unknown expr type:" + expr.kind)
 	}
 }
 
