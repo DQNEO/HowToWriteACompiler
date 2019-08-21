67ef4b53e6db8262db4d84f06dbd777dd8db51b3
DQNEO <dqneoo@gmail.com>
2019-08-21 22:28:09 +0900

    Introduce tokenizer

--- main.go
+++ main.go
@@ -28,12 +28,56 @@ type Token struct {
 	value string
 }
 
-func main() {
-	var source []byte
-	source, _ = ioutil.ReadFile("/dev/stdin")
-	var input string = string(source)
+func readNumber(char byte) string {
+	number := []byte{char}
+	for {
+		char, err := getChar()
+		if err != nil {
+			break
+		}
+		if '0' <= char && char <= '9' {
+			number = append(number, char)
+		} else {
+			ungetChar()
+			break
+		}
+	}
 
-	number, err := strconv.Atoi(input)
+	return string(number)
+}
+
+func tokenize() []*Token {
+	var tokens []*Token
+	fmt.Printf("# Tokens : ")
+
+	for {
+		char, err := getChar()
+		if err != nil {
+			break
+		}
+		switch char {
+		case '0', '1', '2', '3', '4', '5', '6', '7', '8', '9':
+			intliteral := readNumber(char)
+			token := &Token{
+				kind:  "intliteral",
+				value: intliteral,
+			}
+			tokens = append(tokens, token)
+			fmt.Printf(" '%s'", token.value)
+		default:
+			panic(fmt.Sprintf("tokenizer: Invalid char: '%c'", char))
+		}
+	}
+
+	fmt.Printf("\n")
+	return tokens
+}
+
+func main() {
+	source, _ = ioutil.ReadFile("/dev/stdin")
+	tokens := tokenize()
+	token0 := tokens[0]
+	number, err := strconv.Atoi(token0.value)
 	if err != nil {
 		panic(err)
 	}
