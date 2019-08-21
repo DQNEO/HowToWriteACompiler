dac612d19fac4d7ea9f6ed8858c2230037994b8f
DQNEO <dqneoo@gmail.com>
2019-08-21 22:28:09 +0900

    Introduce tokenizer

--- main.go
+++ main.go
@@ -28,13 +28,47 @@ func ungetChar() {
 	sourceIndex--
 }
 
+func tokenize() []*Token {
+	var tokens []*Token
+	for {
+		char, err := getChar()
+		if err != nil {
+			break
+		}
+		switch char {
+		case '0', '1', '2', '3', '4', '5', '6', '7', '8', '9':
+			var number []byte = []byte{char}
+			for {
+				char, err := getChar()
+				if err != nil {
+					break
+				}
+				if '0' <= char && char <= '9' {
+					number = append(number, char)
+				} else {
+					ungetChar()
+					break
+				}
+			}
+			token := &Token{
+				kind:  "intliteral",
+				value: string(number),
+			}
+			tokens = append(tokens, token)
+		default:
+			panic(fmt.Sprintf("tokenizer: Invalid char: '%c'", char))
+		}
+	}
+
+	return tokens
+}
 
 func main() {
 	var err error
-	source, _ := ioutil.ReadFile("/dev/stdin")
-	var input string = string(source)
-
-	number, err := strconv.Atoi(input)
+	source, _ = ioutil.ReadFile("/dev/stdin")
+	tokens := tokenize()
+	token0 := tokens[0]
+	number, err := strconv.Atoi(token0.value)
 	if err != nil {
 		panic(err)
 	}
